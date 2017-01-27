c95f3907ce1aa36064bdd1732ead8ab27e0ce7a3
Ryan Cleghorn <rycleghorn@gmail.com>
2017-01-26 21:26:14 -0500

    updates

---
+++ smartapps/smartthings-users/nest.src/nest.groovy
@@ -0,0 +1,54 @@
+/**
+ *  Nest
+ *
+ *  Copyright 2016 dianoga7@3dgo.net
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Nest",
+    namespace: "smartthings-users",
+    author: "dianoga7@3dgo.net",
+    description: "The brain",
+    category: "SmartThings Labs",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: [displayName: "Ryan Cleghorn", displayLink: "Nest thermostat"]) {
+    appSetting "Ryclegman"
+    appSetting "Ryclegman"
+}
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
